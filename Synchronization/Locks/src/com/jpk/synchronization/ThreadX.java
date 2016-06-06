1929293e2f447c2d0174a3045fd32c335b6a0c70
Piotrek <Piotrek@PC.Home>
2016-06-06 23:58:39 +0100

    Locks

---
+++ Synchronization/Locks/src/com/jpk/synchronization/ThreadX.java
@@ -0,0 +1,12 @@
+package com.jpk.synchronization;
+
+public class ThreadX implements Runnable
+{
+
+    @Override
+    public void run()
+    {
+        // TODO Auto-generated method stub  
+    }
+
+}
